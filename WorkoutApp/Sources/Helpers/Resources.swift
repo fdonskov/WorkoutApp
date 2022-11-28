152899bacacab68d8dc16b652eb98eb78cc835be
fdonskov <fedya_donskov@mail.ru>
2022-11-28 07:26:56 +0300

    Created tabBar

---
+++ WorkoutApp/Sources/Helpers/Resources.swift
@@ -0,0 +1,35 @@
+//
+//  Resources.swift
+//  WorkoutApp
+//
+//  Created by Федор Донсков on 25.11.2022.
+//
+
+import UIKit
+
+enum Resources {
+    enum Colors {
+        static var active = UIColor(hexString: "#437BFE")
+        static var inActive = UIColor(hexString: "#929DA5")
+        
+        static var separator = UIColor(hexString: "#E8ECEF")
+    }
+    
+    enum Strings {
+        enum TabBar {
+            static var overview = "Overview"
+            static var session = "Session"
+            static var progress = "Progress"
+            static var settings = "Settings"
+        }
+    }
+    
+    enum Images {
+        enum TabBar {
+            static var overview = UIImage(named: "overview_tab")
+            static var session = UIImage(named: "session_tab")
+            static var progress = UIImage(named: "progress_tab")
+            static var settings = UIImage(named: "settings_tab")
+        }
+    }
+}
