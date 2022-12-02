4d2364a105afc0fabf2d9945db529c189143a38e
fdonskov <fedya_donskov@mail.ru>
2022-12-02 07:05:03 +0300

    Added a view for an animated timer

---
+++ WorkoutApp/Sources/Controllers/Session/Views/Timer/ProgressView.swift
@@ -0,0 +1,18 @@
+//
+//  ProgressView.swift
+//  WorkoutApp
+//
+//  Created by Федор Донсков on 02.12.2022.
+//
+
+import UIKit
+
+extension TimerView {
+    final class ProgressView: UIView {
+        func drawProgress(with percent: CGFloat) {
+            
+        }
+    }
+}
+
+
---
+++ WorkoutApp/Sources/Controllers/Session/Views/Timer/TimerView.swift
@@ -0,0 +1,43 @@
+//
+//  TimerView.swift
+//  WorkoutApp
+//
+//  Created by Федор Донсков on 02.12.2022.
+//
+
+import UIKit
+
+final class TimerView: WABaseInfoView {
+    
+    private let progressView: ProgressView = {
+        let view = ProgressView()
+        view.drawProgress(with: 0)
+        return view
+    }()
+}
+
+extension TimerView {
+    
+    override func setupViews() {
+        super.setupViews()
+        
+        setupView(progressView)
+    }
+    
+    override func constraintViews() {
+        super.constraintViews()
+        
+        NSLayoutConstraint.activate([
+            progressView.topAnchor.constraint(equalTo: topAnchor, constant: 40),
+            progressView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 40),
+            progressView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -40),
+            progressView.heightAnchor.constraint(equalTo: progressView.widthAnchor)
+        ])
+    }
+    
+    override func configureAppearance() {
+        super.configureAppearance()
+        
+        progressView.backgroundColor = .red
+    }
+}
